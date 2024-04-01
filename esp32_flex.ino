65f359a1d605f4af6276f77fe4221142e5663e96
ananda04 <46113015+ananda04@users.noreply.github.com>
2024-03-31 20:20:55 -0400

    update code with new freq values

---
+++ esp32_flex.ino
@@ -0,0 +1,36 @@
+#include "pitches.h"
+int sensorPin = 12;
+int sensorPin1 = 13;
+int sensorValue;  // select the input pin for the potentiometer
+int confirmValue;
+int count = 0;
+char bluVal;
+int tone 
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  Serial.begin(9600);
+  delay(1000);
+}
+
+
+void loop() {
+// within specific time created by piecewise func 
+  // write signal to audio jack back into speaker 
+  if (SerialBT.available()) {
+    int toneHz  = SerialBT.read();
+  }
+  tone(SpeakerJack, toneHz, length(toneHz))
+// write val to audio jack then to speaker 
+// -------------------------------------------------------------------
+// reading voltage vals and then sending back 
+  sensorValue = analogRead(sensorPin);
+  confirm = analogRead(sensorPin1)
+
+  if(sensorValue == 0){
+    blueVal += 'b';
+  }
+  if(confirm == 0){
+    blueVal += 'c';
+  }
+  delay(500);
+}
